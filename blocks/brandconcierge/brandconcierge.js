3620065c049e01798cf326c327aeff7b2bd2b56c
Wouter Van Geluwe <vangeluw@adobe.com>
2026-04-02 10:09:56 +0200

    del

--- blocks/brandconcierge/brandconcierge.js
+++
@@ -1,6 +0,0 @@
-import { startBrandConciergeAlloy } from '../../scripts/brand-concierge-alloy.js';
-
-export default function decorate(block) {
-  block.setAttribute('id', 'brand-concierge-mount');
-  startBrandConciergeAlloy();
-}
